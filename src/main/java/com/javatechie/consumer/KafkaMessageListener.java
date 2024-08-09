6f307461a604bf0ca9e67d790bd09d8eb6080adf
souleye <sulaynd@gmail.com>
2024-08-09 13:33:48 -0400

    add kafka testContainers

--- src/main/java/com/javatechie/consumer/KafkaMessageListener.java
+++ src/main/java/com/javatechie/consumer/KafkaMessageListener.java
@@ -13,28 +13,28 @@ public class KafkaMessageListener {
 
    // Logger log = LoggerFactory.getLogger(KafkaMessageListener.class);
 
-//    @KafkaListener(topics = "louly-topic",groupId = "ls-group")
-//    public void consumeEvents(Customer customer) {
-//        log.info("consumer consume the events {} ", customer.toString());
+    @KafkaListener(topics = "louly-topic-demo-3",groupId = "jt-group-3")
+    public void consumeEvents(Customer customer) {
+        log.info("consumeEvents consume the events {} ", customer.toString());
+    }
+
+//    @KafkaListener(topics = "louly-topic-demo-1",groupId = "jt-group-2")
+//    public void consume_1(String message) {
+//        log.info("consumer_1 consume the message {} ", message);
 //    }
-
-    @KafkaListener(topics = "louly-topic-demo-1",groupId = "jt-group-2")
-    public void consume_1(String message) {
-        log.info("consumer_1 consume the message {} ", message);
-    }
-
-    @KafkaListener(topics = "louly-topic-demo-1",groupId = "jt-group-2")
-    public void consume_2(String message) {
-        log.info("consumer_2 consume the message {} ", message);
-    }
-
-    @KafkaListener(topics = "louly-topic-demo-1",groupId = "jt-group-2")
-    public void consume_3(String message) {
-        log.info("consumer_3 consume the message {} ", message);
-    }
-
-    @KafkaListener(topics = "louly-topic-demo-1",groupId = "jt-group-2")
-    public void consume_4(String message) {
-        log.info("consumer_4 consume the message {} ", message);
-    }
+//
+//    @KafkaListener(topics = "louly-topic-demo-1",groupId = "jt-group-2")
+//    public void consume_2(String message) {
+//        log.info("consumer_2 consume the message {} ", message);
+//    }
+//
+//    @KafkaListener(topics = "louly-topic-demo-1",groupId = "jt-group-2")
+//    public void consume_3(String message) {
+//        log.info("consumer_3 consume the message {} ", message);
+//    }
+//
+//    @KafkaListener(topics = "louly-topic-demo-1",groupId = "jt-group-2")
+//    public void consume_4(String message) {
+//        log.info("consumer_4 consume the message {} ", message);
+//    }
 }
